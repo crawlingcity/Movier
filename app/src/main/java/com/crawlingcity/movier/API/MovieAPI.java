cfe05967693fbb615237826d8025e845c29b17c0
crawlingcity <crawlingcity@gmail.com>
2016-04-04 02:09:25 +0100

    settings now works

--- app/src/main/java/com/crawlingcity/movier/API/MovieAPI.java
+++ app/src/main/java/com/crawlingcity/movier/API/MovieAPI.java
@@ -9,9 +9,10 @@ import retrofit2.http.Query;
 
 /**
  * Created by crawl on 03/04/2016.
+ * ${PACKAGE_NAME}
  */
 public interface MovieAPI {
-
+//hi
     String BASE_URL ="http://api.themoviedb.org/3/";
     @GET("movie/{endpoint}")
     Call<Movie> movies(@Path("endpoint") String endpoint,
